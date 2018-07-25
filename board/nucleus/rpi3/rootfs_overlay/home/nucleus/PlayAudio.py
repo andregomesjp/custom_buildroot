acfe0996dd29960579b2d7af52315d2de086bf85
andre gomes <andregomesjp@gmail.com>
2018-07-25 19:54:05 -0300

    Fix audio player

--- board/nucleus/rpi3/rootfs_overlay/home/nucleus/PlayAudio.py
+++ board/nucleus/rpi3/rootfs_overlay/home/nucleus/PlayAudio.py
@@ -2,15 +2,15 @@ import subprocess
 from gtts import gTTS
 import dbus.service
 import threading
+import time
 
-player = 'mpg321'
+player = 'mpg123'
 
 class AudioThread(object):
     def __init__(self, audio_text):
         #self.finished = threading.Event()
         self.thread = threading.Thread(target=self.work, args=(audio_text,))
-        self.thread.start()
-
+        self.thread.run()
     def work(self, audio_text):
         try:
             voz = gTTS(text=audio_text, lang='pt')
@@ -33,4 +33,3 @@ class PlayAudio(dbus.service.Object):
             return 1
         except:
             return 0
-
